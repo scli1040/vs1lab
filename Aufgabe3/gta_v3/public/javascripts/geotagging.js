b71f00873dee05ae616f2355a72e7e27c4e55168
Sebastian <73029025+sebiskus@users.noreply.github.com>
2025-11-28 16:24:43 +0100

    Vorbereitung `Aufgabe3` Dateien rüberkopieren, index.ejs bearbeitet

---
+++ Aufgabe3/gta_v3/public/javascripts/geotagging.js
@@ -0,0 +1,190 @@
+// File origin: VS1LAB A2
+
+/* eslint-disable no-unused-vars */
+
+// This script is executed when the browser loads index.html.
+
+// "console.log" writes to the browser's console. 
+// The console window must be opened explicitly in the browser.
+// Try to find this output in the browser...
+console.log("The geoTagging script is going to start...");
+
+/**
+  * A class to help using the HTML5 Geolocation API.
+  */
+class LocationHelper {
+    // Location values for latitude and longitude are private properties to protect them from changes.
+    #latitude = '';
+
+    /**
+     * Getter method allows read access to privat location property.
+     */
+    get latitude() {
+        return this.#latitude;
+    }
+
+    #longitude = '';
+
+    get longitude() {
+        return this.#longitude;
+    }
+
+   /**
+    * Create LocationHelper instance if coordinates are known.
+    * @param {string} latitude 
+    * @param {string} longitude 
+    */
+   constructor(latitude, longitude) {
+       this.#latitude = (parseFloat(latitude)).toFixed(5);
+       this.#longitude = (parseFloat(longitude)).toFixed(5);
+   }
+
+    /**
+     * The 'findLocation' method requests the current location details through the geolocation API.
+     * It is a static method that should be used to obtain an instance of LocationHelper.
+     * Throws an exception if the geolocation API is not available.
+     * @param {*} callback a function that will be called with a LocationHelper instance as parameter, that has the current location details
+     */
+    static findLocation(callback) {
+        const geoLocationApi = navigator.geolocation;
+
+        if (!geoLocationApi) {
+            throw new Error("The GeoLocation API is unavailable.");
+        }
+
+        // Call to the HTML5 geolocation API.
+        // Takes a first callback function as argument that is called in case of success.
+        // Second callback is optional for handling errors.
+        // These callbacks are given as arrow function expressions.
+        geoLocationApi.getCurrentPosition((location) => {
+            // Create and initialize LocationHelper object.
+            let helper = new LocationHelper(location.coords.latitude, location.coords.longitude);
+            // Pass the locationHelper object to the callback.
+            callback(helper);
+        }, (error) => {
+           alert(error.message)
+        });
+    }
+}
+
+/**
+ * A class to help using the Leaflet map service.
+ */
+class MapManager {
+
+    #map
+    #markers
+
+    /**
+    * Initialize a Leaflet map
+    * @param {number} latitude The map center latitude
+    * @param {number} longitude The map center longitude
+    * @param {number} zoom The map zoom, defaults to 18
+    */
+    initMap(latitude, longitude, zoom = 18) {
+        // set up dynamic Leaflet map
+        this.#map = L.map('map').setView([latitude, longitude], zoom);
+        var mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+        L.tileLayer(
+            'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; ' + mapLink + ' Contributors'}).addTo(this.#map);
+        this.#markers = L.layerGroup().addTo(this.#map);
+    }
+
+    /**
+    * Update the Markers of a Leaflet map
+    * @param {number} latitude The map center latitude
+    * @param {number} longitude The map center longitude
+    * @param {{latitude, longitude, name}[]} tags The map tags, defaults to just the current location
+    */
+    updateMarkers(latitude, longitude, tags = []) {
+        // delete all markers
+        this.#markers.clearLayers();
+        L.marker([latitude, longitude])
+            .bindPopup("Your Location")
+            .addTo(this.#markers);
+        for (const tag of tags) {
+            L.marker([tag.latitude,tag.longitude])
+                .bindPopup(tag.name)
+                .addTo(this.#markers);  
+        }
+    }
+}
+
+/**
+ * TODO: 'updateLocation'
+ * A function to retrieve the current location and update the page.
+ * It is called once the page has been fully loaded.
+ */
+// ... your code here ...
+
+function updateLocation(){
+    /* Teilaufgabe 1 */
+    try {
+        LocationHelper.findLocation((helper) => {
+            var latitude = helper.latitude;
+            var longitude = helper.longitude;
+
+            //visible inputs
+            var inputLatitude = document.getElementById("latitude");
+            var inputLongitude = document.getElementById("longitude");
+
+            if (inputLatitude) {
+                inputLatitude.value = latitude;
+            }
+            if (inputLongitude) {
+                inputLongitude.value = longitude;
+            }
+
+            //hidden inputs
+            var discLatInput = document.getElementById("discovery-latitude");
+            var discLonInput = document.getElementById("discovery-longitude");
+
+            if (discLatInput) {
+                discLatInput.value = latitude;
+            }
+            if (discLonInput) {
+                discLonInput.value = longitude;
+            }    
+
+            console.log("Location aktualisiert: " + latitude + ", " + longitude); //Testnachricht
+
+
+            /* Teilaufgabe 2 */
+            try {
+                //trso1014
+                //const -> var stays local and can not accept new value to prevent bugs
+                const mapManager = new MapManager();
+                //map init and own position as marker
+                mapManager.initMap(latitude, longitude);
+                mapManager.updateMarkers(latitude, longitude);
+            
+                //wiph1019
+                // ruft über DOM image, das preset Bild, p und span auf
+                var image = document.getElementById("map");
+                var presetImage = image.getElementsByTagName("img")[0];
+                var p = image.getElementsByTagName("p")[0];
+                var span = image.getElementsByTagName("span")[0];
+
+                // Vorheriges Bild + vorherige Werte werden entfernt
+                presetImage.remove();
+                image.removeChild(span); 
+                if (p) {image.removeChild(p);} // var p existiert bei uns nicht, deswegen if falls es doch vorkommt
+
+                console.log("Map aktualisiert"); //Testnachricht
+            } catch (error) {
+                console.log("Fehler beim Erstellen der Map");
+            }
+    });
+    } catch (error) {
+            console.log("Fehler beim Bestimmen der location");
+        }
+
+}
+
+
+//trso1014
+//function called after all  HTML contents were loaded
+document.addEventListener("DOMContentLoaded", () => {
+    updateLocation();   
+});
